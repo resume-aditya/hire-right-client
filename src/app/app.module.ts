74de55ed326d4adddf25b8023035040f5f95600c
Aditya Sharma <a.aditya.s.sharma@oracle.com>
2021-09-21 00:29:55 +0530

    login logic

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { Router, RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginHeaderComponent } from './login/login-header/login-header.component';
 import { LoginContentComponent } from './login/login-content/login-content.component';
 import { ShowcaseTilesComponent } from './login/showcase-tiles/showcase-tiles.component';
+import { HomePanelComponent } from './dashboard/landing/home-panel/home-panel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginHeaderComponent,
     LoginContentComponent,
-    ShowcaseTilesComponent
+    ShowcaseTilesComponent,
+    HomePanelComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([{
+      path : 'login',
+      component : LoginContentComponent
+    },{
+      path : 'home',
+      component : HomePanelComponent
+    },{
+      path : '',
+      redirectTo : 'login',
+      pathMatch : 'full'
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login-header/login-header.component.ts
+++ src/app/login/login-header/login-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-header',
@@ -7,9 +9,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginHeaderComponent implements OnInit {
 
-  constructor() { }
+  buttonText: string = 'Sign In';
+  username: string;
+  password: string;
+  userExists: boolean;
+  loginTimer;
+  stopLogin: boolean = true;
+  constructor(private http:HttpClient,
+    private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  usernameChange(): void {
+    if (!this.username.trim())
+      return;
+    clearTimeout(this.loginTimer);
+    this.loginTimer = setTimeout(() => {
+      this.checkIfUserExists(this.username).subscribe((resp)=>{
+        this.userExists = Boolean(resp);
+        this.buttonText = resp ? 'Sign In' : 'Sign Up';
+        this.stopLogin = false;
+      });
+    }, 500);
+  }
+  checkIfUserExists(username) {
+    this.stopLogin = true;
+    let params = new HttpParams().set("username",username);
+    return this.http.get("http://localhost:3000/login/userExists", {
+      params : params
+    });
+  }
+  doLogin () {
+    let params = new HttpParams().set("username",this.username).set("password",this.password);
+    return this.http.get("http://localhost:3000/login/doLogin", {
+      params : params
+    });
+  }
+  loginSubmit(): void {
+    if (this.userExists) {
+      if (this.password) {
+        this.doLogin().subscribe((resp) => {
+          if (resp) {
+            this.router.navigateByUrl('/home');
+          } else {
+            alert('Under construction : Pls check the password')
+          }
+        });
+      } else {
+        //some password handling
+      }
+    } else {
+      alert('some signup')
+    }
+  }
 }
